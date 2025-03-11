99c35adcb29b8b55a6d62776847251e4dc82a640
Parisa <p.hoseyni75@gmail.com>
2025-03-11 15:01:38 +0330

    Implemented CustomUserDetailsService for authentication

--- src/main/java/com/tosan/security/CustomUserDetailsService.java
+++ src/main/java/com/tosan/security/CustomUserDetailsService.java
@@ -1,2 +1,28 @@
-package com.tosan.security;public class CustomUserDetailsService {
+package com.tosan.security;
+
+import com.tosan.entity.User;
+import com.tosan.exceptions.UserNotFoundException;
+import com.tosan.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            throw new UserNotFoundException("User not found with username: " + username);
+        }
+        return null;
+    }
 }
